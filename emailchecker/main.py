81731649abd33ab35c14373354bfd03f8cb2ddeb
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-08 15:49:39 +0100

    Saving version 1.1.0 before deploying to PyPI.

---
+++ emailchecker/main.py
@@ -0,0 +1,6 @@
+def main():
+    print(f'Running EmailChecker...')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+requirements = []
+with open('requirements.txt', 'r') as f:
+    for line in f.readlines():
+        line = line.strip()
+        requirements.append(line)
+
+setup_requirements = []
+
+test_requirements = []
+
+with open('VERSION') as f:
+    VERSION = f.read()
+
+setup(
+    author="Ralph Brecheisen",
+    author_email='r.brecheisen@maastrichtuniversity.nl',
+    python_requires='>=3.11',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=find_packages(include=[
+        'emailchecker', 
+    ]),
+    include_package_data=True,
+    # package_data={
+    #     'mosamaticdesktop': ['scripts/*', 'VERSION', 'GIT_COMMIT_ID'],
+    # },
+    description="Logging tool exposed through email",
+    install_requires=requirements,
+    license="MIT license",
+    keywords='emailchecker',
+    name='emailchecker',
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    test_suite='tests',
+    entry_points={
+        'console_scripts': [
+            'emailchecker=emailchecker.main:main',
+        ],
+    },
+    version=VERSION,
+    zip_safe=False,
+)
